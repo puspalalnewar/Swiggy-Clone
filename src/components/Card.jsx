a0861a553b5f44239d17d12ffc27b41506d14562
Puspalal <puspalalnewar79@gmail.com>
2024-09-28 22:17:06 +0530

    Added imagees and rendered

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+
+
   return (
-    <div>
+    <div className='w-[273px] mt-8 shrink-0 grow'>
+      <div className="h-[200px] w-[300px] rounded-[15px] overflow-hidden relative">
+        <img className=' h-full w-full' src={props.name} alt="" />
+        <div className='absolute image-overlay w-full h-full top-0 flex items-end p-2 text-[25px] font-bold text-white tracking-tighter'>
+            {props.offer}
+        </div>
+      </div>
       
     </div>
   )
--- src/components/TopRest.jsx
+++ src/components/TopRest.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import Card from './Card';
+import mixture from '../images/res-mixture.jpg'
+import fish from '../images/res-fish.jpeg'
+import fruites from '../images/res-fruites.jpeg'
 
 const TopRest = () => {
+
+    const data = [
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+        },
+        {
+            name: fish,
+            offer : "₹ 50 OF ABOVE at ₹ 199",
+        },
+        {
+            name: fruites,
+            offer : "₹ 70 OF ABOVE at ₹ 249",
+        },
+        {
+            name: mixture,
+            offer : "₹ 80 OF ABOVE at ₹ 349",
+        },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+        },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+        },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+        },
+    ]
+
     return (
         <div className='max-w-[1200px] mx-auto mt-4'>
             <div className='flex items-center justify-between'>
@@ -13,7 +48,17 @@ const TopRest = () => {
                 </div>
 
             </div>
-            <Card/>
+            <div className="flex gap-10 overflow-hidden">
+
+                {
+                    data.map((val, idx) => {
+                        return (
+                            <Card {...val} id = {idx}/>
+                        )
+                    })
+                }
+
+            </div>
         </div>
     )
 }
