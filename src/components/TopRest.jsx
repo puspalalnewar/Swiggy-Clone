1fa7e19c2360e56be4b1fc1094eaade6b6d67d8c
Puspalal <puspalalnewar79@gmail.com>
2024-09-29 22:17:52 +0530

    Completed Slide

--- src/components/TopRest.jsx
+++ src/components/TopRest.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import Card from './Card';
 import mixture from '../images/res-mixture.jpg'
@@ -78,24 +78,86 @@ const TopRest = () => {
             shopName : "Pizzas",
             place : "New Jodhpur",
         },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+            title : "Pizza Hut",
+            rating : 4.2,
+            minTime : 30,
+            maxTime : 45,
+            shopName : "Pizzas",
+            place : "New Jodhpur",
+        },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+            title : "Pizza Hut",
+            rating : 4.2,
+            minTime : 30,
+            maxTime : 45,
+            shopName : "Pizzas",
+            place : "New Jodhpur",
+        },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+            title : "Pizza Hut",
+            rating : 4.2,
+            minTime : 30,
+            maxTime : 45,
+            shopName : "Pizzas",
+            place : "New Jodhpur",
+        },
+        {
+            name: mixture,
+            offer : "Items at ₹ 179",
+            title : "Pizza Hut",
+            rating : 4.2,
+            minTime : 30,
+            maxTime : 45,
+            shopName : "Pizzas",
+            place : "New Jodhpur",
+        },
     ]
+
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => {
+        if (slide >= 9) {
+            return;
+        }
+        setSlide(slide + 3.3);
+        // console.log(data.length);
+    }
+    const prevSlide = () => {
+        if (slide <= 0) {
+            setSlide(0);
+        } else {
+            setSlide(slide - 3.3);
+        }
+
+    }
 
     return (
         <div className='max-w-[1200px] mx-auto mt-4'>
             <div className='flex items-center justify-between'>
                 <div className='text-[25px] font-bold'>Top restaurant chains in Jodhpur</div>
                 <div className='flex'>
-                    <div className='flex justify-center items-center cursor-pointer w-[40px] h-[40px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowLeft /></div>
-                    <div className='flex justify-center items-center  cursor-pointer w-[40px] h-[40px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowRight /></div>
+                    <div onClick={prevSlide} className='flex justify-center items-center cursor-pointer w-[40px] h-[40px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowLeft /></div>
+                    <div onClick={nextSlide} className='flex justify-center items-center  cursor-pointer w-[40px] h-[40px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowRight /></div>
                 </div>
 
             </div>
-            <div className="flex gap-10 overflow-hidden">
+            <div className="flex gap-10 overflow-hidden" >
 
                 {
                     data.map((val, idx) => {
                         return (
-                            <Card {...val} image = {val.name} id = {idx}/>
+                            <div className="box duration-500" style={{
+                                transform: `translateX(-${slide * 100}%)`}}>
+                                <Card {...val} image = {val.name} id = {idx} />
+                            </div>
+                            
                         )
                     })
                 }
